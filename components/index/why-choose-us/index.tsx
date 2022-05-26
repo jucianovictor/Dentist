449ee01c5a70b18a994be7987c03c30683e1b8d7
Juciano Oliveira <juciano.oliveira@seduc.ce.gov.br>
2022-05-26 10:38:39 -0300

    Adicionando funcionalidade no play

--- components/index/why-choose-us/index.tsx
+++ components/index/why-choose-us/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { DefaultTheme } from 'styled-components';
 import Reception from '../../../public/images/recep.png';
 import Span from '../../../styles/template/span/span';
@@ -25,37 +25,25 @@ interface Props {
 	src: string;
 }
 
-const VideoPlayer: React.FC<{ src: string }> = ({ src }: { src: string }) => {
-	return <Video src={src} width="300%" height={300} hidden={true} />;
+const openVideo = (ref: React.RefObject<HTMLVideoElement>) => {
+	ref.current.requestFullscreen().catch(err => console.log('error: ' + err));
 };
 
-const WhyChooseUs: React.FC<Props> = ({ theme, src }: Props) => (
-	<Container>
-		<Header>
-			<Title>
-				<Span color={theme.colors.secondary.light}>Por que </Span>
-				<Span color={theme.colors.primary.default}>nos escolher?</Span>
-			</Title>
-			<SubTitle>
-				is simply dummy text of the printing and typesetting industry. Lorem
-				Ipsum has.
-			</SubTitle>
-		</Header>
-		<ListContainer>
-			{new Array(4).fill(0).map((_, key) => (
-				<Item key={key}>
-					<ItemIcon />
-					<ItemTitle>Confidential</ItemTitle>
-					<ItemContent>
-						is simply dummy text of the printing and typesetting industry. Lorem
-						Ipsum has.
-					</ItemContent>
-				</Item>
-			))}
-		</ListContainer>
+const VideoPlayer: React.FC<Props> = ({ theme, src }) => {
+	const videoRef: React.RefObject<HTMLVideoElement> = useRef();
+
+	useEffect(() => {
+		videoRef.current.onfullscreenchange = () => {
+			videoRef.current.hidden = !videoRef.current.hidden;
+			videoRef.current.currentTime = 0;
+			videoRef.current[videoRef.current.hidden ? 'pause' : 'play']();
+		};
+	}, []);
+
+	return (
 		<VideoContainer>
 			<Picture>
-				<Play aria-label="Play">
+				<Play aria-label="Play" onClick={() => openVideo(videoRef)}>
 					<PlayIcon
 						color={theme.colors.accent.default}
 						width={89}
@@ -71,9 +59,39 @@ const WhyChooseUs: React.FC<Props> = ({ theme, src }: Props) => (
 					layout={'responsive'}
 				/>
 			</Picture>
-			<VideoPlayer src={src} />
+			<Video ref={videoRef} src={src} width="300%" height={300} hidden={true} />
 		</VideoContainer>
-	</Container>
-);
+	);
+};
+
+const WhyChooseUs: React.FC<Props> = ({ theme, src }: Props) => {
+	return (
+		<Container>
+			<Header>
+				<Title>
+					<Span color={theme.colors.secondary.light}>Por que </Span>
+					<Span color={theme.colors.primary.default}>nos escolher?</Span>
+				</Title>
+				<SubTitle>
+					is simply dummy text of the printing and typesetting industry. Lorem
+					Ipsum has.
+				</SubTitle>
+			</Header>
+			<ListContainer>
+				{new Array(4).fill(0).map((_, key) => (
+					<Item key={key}>
+						<ItemIcon />
+						<ItemTitle>Confidential</ItemTitle>
+						<ItemContent>
+							is simply dummy text of the printing and typesetting industry.
+							Lorem Ipsum has.
+						</ItemContent>
+					</Item>
+				))}
+			</ListContainer>
+			<VideoPlayer src={src} theme={theme} />
+		</Container>
+	);
+};
 
 export { WhyChooseUs };
