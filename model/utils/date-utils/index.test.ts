1c1e99481e8ff643d9bb83fd3396d9750acb86cc
Juciano Victor <jucianovictor95@gmail.com>
2022-08-19 13:24:55 -0300

    remove date-utils

--- model/utils/date-utils/index.test.ts
+++
@@ -1,4 +0,0 @@
-import { getNumberOfDaysFromDate } from '.';
-
-it('should return 31 days', () =>
-	expect(getNumberOfDaysFromDate(new Date(2020, 0, 1))).toBe(31));
--- model/utils/date-utils/index.ts
+++
@@ -1,7 +0,0 @@
-export function getNumberOfDaysFromDate(date: Date): number {
-	return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-}
-
-export function getMonthNameFromDate(date: Date, locale = 'default'): string {
-	return date.toLocaleDateString(locale, { month: 'long' });
-}
