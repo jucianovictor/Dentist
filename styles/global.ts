f242b339912058fa85118f4868850c2935bc5fa0
Juciano Victor <jucianovictor95@gmail.com>
2022-08-20 03:39:55 -0300

    add scrollbar style

--- styles/global.ts
+++ styles/global.ts
@@ -16,4 +16,23 @@ export default createGlobalStyle`
     width: 100%;
     height: 100%;
   }
+
+	::-webkit-scrollbar {
+		width: 3px;
+		height: 3px;
+	}
+
+	::-webkit-scrollbar-track {
+		background-color: ${({ theme }) => theme.colors.secondary.light};
+		border-radius: 20px;
+	}
+
+	::-webkit-scrollbar-thumb {
+		background-color: ${({ theme }) => theme.colors.accent.light};
+		border-radius: 20px;
+	}
+
+	::-webkit-scrollbar-thumb:hover {
+		background-color: ${({ theme }) => theme.colors.accent.default};
+	}
 `;
